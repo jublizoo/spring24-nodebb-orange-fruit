c300e5c9375d5f6366d70c6145ddd4206f5807aa
Jam <jjhennessy1231@gmail.com>
2024-02-24 18:22:41 -0500

    Parallelized privilege updates, fixed some linter issues

--- src/privileges/index.js
+++ src/privileges/index.js
@@ -12,33 +12,40 @@ privileges.topics = require('./topics');
 privileges.posts = require('./posts');
 privileges.users = require('./users');
 
-//No parameters, and void return type - no need for such assertions
+// No parameters, and void return type - no need for such assertions
 privileges.init = async () => {
     let uids = await db.getSortedSetRangeWithScores('users:joindate', 0, -1);
-    console.log(typeof uids);
     assert(typeof uids === 'object');
+
     uids = uids.map(user => user.value);
-    console.log(typeof uids);
     assert(typeof uids === 'object');
+    
+    const promises = []
 
-    for (let uid of uids) {
-        assert(typeof uid === 'number');
-        
-        const userInfo = await Users.getUserField(uid, 'accounttype');
-        const isTA = (userInfo === 'TA');
-        const isInstructor = (userInfo === 'instructor');
-        
-        assert(typeof userInfo == 'string');
-        assert(typeof isTA === 'boolean');
-        assert(typeof isInstructor === 'boolean');
+    for (const uid of uids) {
+        promises.push((async () => {
+            assert(typeof uid === 'number');
 
-        if (isTA || isInstructor) {
-            privileges.global.give(['mute'], [uid]);
-        }
-        if (isInstructor) {
-            privileges.global.give(['ban'], [uid])
-        }
-    } 
+            const userInfo = await Users.getUserField(uid, 'accounttype');
+            const isTA = (userInfo === 'TA');
+            const isInstructor = (userInfo === 'instructor');
+
+            assert(typeof userInfo === 'string');
+            assert(typeof isTA === 'boolean');
+            assert(typeof isInstructor === 'boolean');
+
+            if (isTA || isInstructor) {
+                privileges.global.give(['mute'], [uid]);
+            }
+            if (isInstructor) {
+                privileges.global.give(['ban'], [uid]);
+            }
+        })());
+    }
+
+    assert(typeof promises === 'object');
+    
+    await Promise.all(promises);
 
     await privileges.global.init();
     await privileges.admin.init();
